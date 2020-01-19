972c80a84f9f7ad410f4557139679b20e4b21c9f
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 03:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function greet(input) {
   else {
     let greeting = 'Good Evening';
   }
-  return greeting;
+  return wordArray;
 }
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
