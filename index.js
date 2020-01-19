4b16e2760ce51d83e7267f509911d768f9c35794
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 03:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ function handleClick(e) {
 function greet(input) {
   let wordArray = input.split(':');
   let num = parseInt(wordArray, 10);
-  let greeting = null;
   if (num[0] < 12) {
-    greeting = 'Good Morning';
+    let greeting = 'Good Morning';
   }
   else if (num[0] >= 12 && num[0] <= 5) {
-    greeting = 'Good Afternoon';
+    let greeting = 'Good Afternoon';
   }
   else {
-    greeting = 'Good Evening';
+    let greeting = 'Good Evening';
   }
   return greeting;
 }
