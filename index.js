715fb4a83019f9109f1c72da81668331ffffcf39
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 03:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ function handleClick(e) {
 function greet(input) {
   wordArray = input.split(':');
   input = parseInt(wordArray, 10);
-
+   let greeting = null;
+  if (wordArray[0] < 12) {
+    greeting = 'Good Morning';
+  }
+  else if (wordArray[0] >= 12 && wordArray[0] <= 5) {
+    greeting = 'Good Afternoon'
+  }
+  else {
+    greeting = 'Good Evening';
+  }
+  return greeting;
 }
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
